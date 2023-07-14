f57abaacbc43fb39b6e62f5d98a44defbc6468e0
Maxibarto87 <barto_02_07_87@outlook.com>
2023-07-14 16:36:18 -0300

    Se agrego archivo index y crear en proveedor

--- secciones/proveedor/crear.php
+++ secciones/proveedor/crear.php
@@ -1,3 +1,37 @@
 <?php require_once("../../templates/header.php") ?>
-    Crear Proveedor
+<div class="card">
+        <div class="card-header">
+            Datos de Proveedores
+        </div>
+        <div class="card-body">
+            <form action="" method="post">
+                <div class="mb-3">
+                  <label for="pr_Nombre_de_fantasia" class="form-label">Nombre de Fantasia</label>
+                  <input type="text" class="form-control" name="pr_Nombre_de_fantasia" id="pr_Nombre_de_fantasia" aria-describedby="helpId" placeholder="Ingrese su Nombre de Fantasia">  
+                        <br>
+                  <label for="pr_Nombre" class="form-label">Nombre</label>
+                  <input type="text" class="form-control" name="pr_Nombre" id="pr_Nombre" aria-describedby="helpId" placeholder="Ingrese su Nombre">
+                        <br>
+                  <label for="pr_Apellido" class="form-label">Apellido</label>
+                  <input type="text" class="form-control" name="pr_Apellido" id="pr_Apellido" aria-describedby="helpId" placeholder="Ingrese su Apellido">
+                        <br>
+                  <label for="pr_Email" class="form-label">Email</label>
+                  <input type="email" class="form-control" name="pr_Email" id="pr_Email" aria-describedby="helpId" placeholder="Ingrese su Email">
+                        <br>
+                  <label for="pr_Direccion" class="form-label">Dirección</label>
+                  <input type="text" class="form-control" name="pr_Direccion" id="pr_Direccion" aria-describedby="helpId" placeholder="Ingrese su Dirección">
+                        <br>
+                  <label for="pr_CUIT" class="form-label">CUIT</label>
+                  <input type="text" class="form-control" name="pr_CUIT" id="pr_CUIT" aria-describedby="helpId" placeholder="Ingrese Nombre de Categoria">
+                        <br>
+                  <label for="pr_Telefono" class="form-label">Teléfono</label>
+                  <input type="text" class="form-control" name="pr_Telefono" id="pr_Telefono" aria-describedby="helpId" placeholder="Ingrese Descripcion de Categoria">
+                        <br>
+                 <button type="submit" name="" id="" class="btn btn-primary" role="button">Enviar</button>       
+                 <a name="" id="" class="btn btn-danger" href="./index.php" role="button">cancelar</a> <!-- para agregar la direccion se hace asi -->
+                </div>
+            </form>
+        </div>
+        
+    </div>
 <?php require_once("../../templates/footer.php") ?>
--- secciones/proveedor/index.php
+++ secciones/proveedor/index.php
@@ -1,3 +1,48 @@
 <?php require_once("../../templates/header.php") ?>
-    Mostrar Proveedor
+<h1>Proveedor</h1>   
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="./crear.php" role="button">Agregar Proveedor</a>
+    </div>
+    <div class="card-body">
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre de fantasía</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Dirección</th>
+                    <th scope="col">CUIT</th>
+                    <th scope="col">Telefono</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row">ID</td>
+                    <td>Nombre de fantasía</td>
+                    <td>Nombre</td>
+                    <td>Apellido</td>
+                    <td>Email</td>
+                    <td>Dirección</td>
+                    <td>Cuit</td>
+                    <td>Telefono</td>
+                    <td>
+                        <a name="" id="" class="btn btn-info" href="#" role="button">Editar</a> 
+                        <a name="" id="" class="btn btn-danger" href="#" role="button">Eliminar</a>
+                    </td>
+                        
+                
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+    
+    
+</div> 
+</div> 
 <?php require_once("../../templates/footer.php") ?>
