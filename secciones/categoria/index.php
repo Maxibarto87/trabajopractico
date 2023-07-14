d4cca53bec5a38dcdda4223b1e78c67130fc4e80
Maxibarto87 <barto_02_07_87@outlook.com>
2023-07-14 09:20:38 -0300

    se agrego tabla

--- secciones/categoria/index.php
+++ secciones/categoria/index.php
@@ -1,3 +1,32 @@
 <?php require_once("../../templates/header.php") ?>
     Mostrar Categoria
+
+<div class="card">
+    <div class="card-header">
+        Categoria
+    </div>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre Categoria</th>
+                    <th scope="col">Descripcion Categoria</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row">ID</td>
+                    <td>NOMBRE CATEGORIA</td>
+                    <td>DESCRIPCION CATEGORIA</td>
+                    <td>Editar | Eliminar</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+    
+    
+</div>    
 <?php require_once("../../templates/footer.php") ?>
