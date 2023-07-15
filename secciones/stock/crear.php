67c799c7fb384d414a988101506fd3075dc950f2
Maxibarto87 <barto_02_07_87@outlook.com>
2023-07-15 09:31:44 -0300

    Se corrigio stock

--- secciones/stock/crear.php
+++ secciones/stock/crear.php
@@ -1,31 +1,61 @@
-<?php require_once("../../templates/header.php") ?>
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    require_once("../../libs/conexion.php");
+
+    // Recolectar datos del método POST
+    $st_Nombre_del_producto = isset($_POST["st_Nombre_del_producto"]) ? $_POST["st_Nombre_del_producto"] : "";
+    $st_Precio_por_unidad = isset($_POST["st_Precio_por_unidad"]) ? $_POST["st_Precio_por_unidad"] : "";
+    $st_N_de_existencia = isset($_POST["st_N_de_existencia"]) ? $_POST["st_N_de_existencia"] : "";
+    $st_Categoria = isset($_POST["st_Categoria"]) ? $_POST["st_Categoria"] : "";
+    $st_Cantidad_total_en_existencia = isset($_POST["st_Cantidad_total_en_existencia"]) ? $_POST["st_Cantidad_total_en_existencia"] : "";
+
+    // Preparar la inserción de datos
+    $sentencia = $conexion->prepare("INSERT INTO stock (st_Nombre_del_producto, st_Precio_por_unidad, st_N_de_existencia, st_Categoria, st_Cantidad_total_en_existencia) VALUES (:st_Nombre_del_producto, :st_Precio_por_unidad, :st_N_de_existencia, :st_Categoria, :st_Cantidad_total_en_existencia)");
+
+    $sentencia->bindParam(":st_Nombre_del_producto", $st_Nombre_del_producto);
+    $sentencia->bindParam(":st_Precio_por_unidad", $st_Precio_por_unidad);
+    $sentencia->bindParam(":st_N_de_existencia", $st_N_de_existencia);
+    $sentencia->bindParam(":st_Categoria", $st_Categoria);
+    $sentencia->bindParam(":st_Cantidad_total_en_existencia", $st_Cantidad_total_en_existencia);
+
+    if ($sentencia->execute()) {
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Error al ejecutar la consulta";
+    }
+}
+
+?>
+
+<?php require_once("../../templates/header.php"); ?>
 <div class="card">
-        <div class="card-header">
-            Datos de Stock
-        </div>
-        <div class="card-body">
-            <form action="" method="post">
-                <div class="mb-3">
-                  <label for="st_Nombre_del_producto" class="form-label">Nombre del Producto</label>
-                  <input type="text" class="form-control" name="st_Nombre_del_producto" id="st_Nombre_del_producto" aria-describedby="helpId" placeholder="Ingrese el nombre de producto">  
-                        <br>
-                  <label for="st_Precio_por_unidad" class="form-label">Precio por unidad</label>
-                  <input type="number" class="form-control" name="st_Precio_por_unidad" id="st_Precio_por_unidad" aria-describedby="helpId" placeholder="Ingrese su precio por unidad">
-                        <br>
-                  <label for="st_N_de_existencia" class="form-label">Numero de existencia</label>
-                  <input type="number" class="form-control" name="st_N_de_existencia" id="st_N_de_existencia" aria-describedby="helpId" placeholder="Ingrese su numero de existencia">
-                        <br>
-                  <label for="st_Categoria" class="form-label">Categoria</label>
-                  <input type="text" class="form-control" name="	st_Categoria" id="	st_Categoria" aria-describedby="helpId" placeholder="Ingrese su categoria">
-                        <br>
-                  <label for="st_Cantidad_total_en_existencia" class="form-label">Cantidad total en existencia</label>
-                  <input type="number" class="form-control" name="st_Cantidad_total_en_existencia" id="st_Cantidad_total_en_existencia" aria-describedby="helpId" placeholder="Ingrese cantidad total en existencia">
-                        <br>
-                 <button type="submit" name="" id="" class="btn btn-primary" role="button">Enviar</button>       
-                 <a name="" id="" class="btn btn-danger" href="./index.php" role="button">cancelar</a> <!-- para agregar la direccion se hace asi -->
-                </div>
-            </form>
-        </div>
-        
+    <div class="card-header">
+        Datos de Stock
     </div>
-<?php require_once("../../templates/footer.php") ?>
+    <div class="card-body">
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="st_Nombre_del_producto" class="form-label">Nombre del Producto</label>
+                <input type="text" class="form-control" name="st_Nombre_del_producto" id="st_Nombre_del_producto" placeholder="Ingrese el nombre del producto">
+                <br>
+                <label for="st_Precio_por_unidad" class="form-label">Precio por unidad</label>
+                <input type="number" class="form-control" name="st_Precio_por_unidad" id="st_Precio_por_unidad" placeholder="Ingrese su precio por unidad">
+                <br>
+                <label for="st_N_de_existencia" class="form-label">Número de existencia</label>
+                <input type="number" class="form-control" name="st_N_de_existencia" id="st_N_de_existencia" placeholder="Ingrese su número de existencia">
+                <br>
+                <label for="st_Categoria" class="form-label">Categoría</label>
+                <input type="text" class="form-control" name="st_Categoria" id="st_Categoria" placeholder="Ingrese su categoría">
+                <br>
+                <label for="st_Cantidad_total_en_existencia" class="form-label">Cantidad total en existencia</label>
+                <input type="number" class="form-control" name="st_Cantidad_total_en_existencia" id="st_Cantidad_total_en_existencia" placeholder="Ingrese la cantidad total en existencia">
+                <br>
+                <button type="submit" name="submit" class="btn btn-primary" role="button">Enviar</button>
+                <a name="" id="" class="btn btn-danger" href="./index.php" role="button">Cancelar</a>
+            </div>
+        </form>
+    </div>
+</div>
+<?php require_once("../../templates/footer.php"); ?>
--- secciones/stock/index.php
+++ secciones/stock/index.php
@@ -1,72 +1,74 @@
-<?php require_once("../../libs/conexion.php");
+<?php
 
-$sentencia=$conexion->prepare("SELECT * FROM `stock`");
+require_once("../../libs/conexion.php");
+
+if (isset($_GET["txtID"])) {
+    // Recolectar datos con método GET
+    $txtID = (isset($_GET["txtID"]) ? $_GET["txtID"] : "");
+    $sentencia = $conexion->prepare("DELETE FROM `stock` WHERE `id_st` = :id_st");
+    // Asignar los valores que vienen del método GET
+    $sentencia->bindParam(":id_st", $txtID);
+    $sentencia->execute();
+    header("Location: index.php");
+    exit();
+}
+
+$sentencia = $conexion->prepare("SELECT * FROM `stock`");
 $sentencia->execute();
 $lista_stock = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($lista_stock)
-
 ?>
 
-<?php require_once("../../templates/header.php") ?>
-<h1>Stock</h1>   
+<?php require_once("../../templates/header.php"); ?>
+<h1>Stock</h1>
 <div class="card">
     <div class="card-header">
         <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar Stock</a>
     </div>
     <div class="card-body">
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nombre del producto</th>
-                    <th scope="col">Precio por unidad</th>
-                    <th scope="col">Numero de existencia</th>
-                    <th scope="col">Categoria</th>
-                    <th scope="col">Cantidad total en existencia</th>
-                    <th scope="col">Acciones</th>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($lista_stock as $registro) {?>
-    
-
-                <tr class="">
-                    <td scope="row">
-                        <?php echo $registro['id_st']; ?>
-                    </td>
-                    <td>
-                        <?php echo $registro['st_Nombre_del_producto']; ?>
-                    </td>
-                    <td>
-                        <?php echo $registro['st_Precio_por_unidad']; ?>
-                    </td>
-                    <td>
-                        <?php echo $registro['st_N_de_existencia']; ?>
-                    </td>
-                    <td>
-                        <?php echo $registro['st_Categoria']; ?>
-                    </td>
-                    <td>
-                        <?php echo $registro['st_Cantidad_total_en_existencia']; ?>
-                    </td>
-                    <td>
-                        <a name="" id="" class="btn btn-info" href="#" role="button">Editar</a> 
-                        <a name="" id="" class="btn btn-danger" href="#" role="button">Eliminar</a>
-                    </td>
-                        
-                
-                </tr>
-                <?php }?>
-    
-        
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombre del producto</th>
+                        <th scope="col">Precio por unidad</th>
+                        <th scope="col">Numero de existencia</th>
+                        <th scope="col">Categoria</th>
+                        <th scope="col">Cantidad total en existencia</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($lista_stock as $registro) { ?>
+                        <tr>
+                            <td scope="row">
+                                <?php echo $registro['id_st']; ?>
+                            </td>
+                            <td>
+                                <?php echo $registro['st_Nombre_del_producto']; ?>
+                            </td>
+                            <td>
+                                <?php echo $registro['st_Precio_por_unidad']; ?>
+                            </td>
+                            <td>
+                                <?php echo $registro['st_N_de_existencia']; ?>
+                            </td>
+                            <td>
+                                <?php echo $registro['st_Categoria']; ?>
+                            </td>
+                            <td>
+                                <?php echo $registro['st_Cantidad_total_en_existencia']; ?>
+                            </td>
+                            <td>
+                                <a name="" id="" class="btn btn-info" href="#" role="button">Editar</a>
+                                <a name="" id="" class="btn btn-danger" href="index.php?txtID=<?php echo $registro['id_st']; ?>" role="button">Eliminar</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-    
-    
-</div> 
-</div>  
-<?php require_once("../../templates/footer.php") ?>
+</div>
+<?php require_once("../../templates/footer.php"); ?>
