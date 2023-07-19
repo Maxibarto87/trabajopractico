08d99a59cb40758255b6ac2f2277bcdc32c93b20
Maxibarto87 <barto_02_07_87@outlook.com>
2023-07-19 13:23:54 -0300

    arreglos en index

--- libs/index.php
+++ libs/index.php
@@ -1,10 +1,12 @@
-<?php require_once("../templates/header.php") ?>
+<?php
+session_start();
+require_once("../templates/header.php") ?>
 <br>
 <div class="p-5 mb-4 bg-light rounded-3">
-   <div class="container-fluid py-5">
-        <h1 class="display-5 fw-bold">BIENVENIDOS</h1>
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Bienvenid@ al sistema</h1>
         <p class="col-md-8 fs-4">
-        Todo lo que queres saber esta aca!!
+            <?php echo $_SESSION['usuario_us']; ?>
         </p>
         <button class="btn btn-primary btn-lg" type="button">Inicio</button>
     </div>
--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@ $url_base = "http://localhost/app/"
 <html lang="en">
 
 <head>
-<title>Administracion</title>
+<title>Administraciòn</title>
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
