90cb2c0b35df233fe7645786e28dc438eb959981
Maxibarto87 <barto_02_07_87@outlook.com>
2023-07-23 22:01:49 -0300

    se agrego hojas de estilo

--- templates/header.php
+++ templates/header.php
@@ -12,26 +12,26 @@ $url_base = "http://localhost/app/";
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
 <!-- Bootstrap CSS v5.2.1 -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
   crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css" />
-  <link rel="stylesheet" href="../libs/style.css">
-  
+  <link rel="stylesheet" href="../templates/style.css">
+  <link rel="icon" href="../assets/10wmt-articleLarge-v4.png">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script>
  
 </head>
 
 <body>
-<header class="header">
+<header>
     <nav class="navbar navbar-expand navbar-light bg-light">
         <ul class="nav navbar-nav">
             <li class="nav-item">
-                <a class="nav-link active" href="<?php echo $url_base ?>libs/login.php" aria-current="page">Inicio<span class="visually-hidden">(current)</span></a>
+                <b></b><a class="nav-link active" href="<?php echo $url_base ?>libs/login.php" aria-current="page">Inicio<span class="visually-hidden">(current)</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo $url_base?>secciones/stock">Stock</a>
