0412179de91f53df8e58a7d8e7515cb02256a65b
Maxibarto87 <barto_02_07_87@outlook.com>
2023-07-25 16:09:36 -0300

    correccion div

--- libs/index.php
+++ libs/index.php
@@ -3,14 +3,14 @@ session_start();
 require_once("../templates/header.php");
 ?>
 <br>
-<div class="p-5 mb-4 bg-light rounded-3">
-    <div class="container-fluid py-5">
+<div class="container-fluid py-5">
         <h1 class="display-5 fw-bold">Bienvenid@ al sistema</h1>
         <p class="col-md-8 fs-4">
             <?php echo $_SESSION['usuario_us']; ?>
         </p>
-        <button class="btn btn-primary btn-lg" type="button">Inicio</button>
-    </div>
+        <!-- <button class="btn btn-primary btn-lg" type="button">Inicio</button> -->
+   
 </div>
+
    <img src="../assets/img/Administración.jpg" alt="Esta es una foto de administracion" class="img">
 <?php require_once("../templates/footer.php"); ?>
