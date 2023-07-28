7122e3ff7be010871e113d205df8f8526eb9d59a
Maxibarto87 <barto_02_07_87@outlook.com>
2023-07-28 14:11:27 -0300

    se agrego actualizaciones a style

--- templates/header.php
+++ templates/header.php
@@ -31,7 +31,7 @@ $url_base = "http://localhost/app/";
     <nav class="navbar navbar-expand navbar-light bg-light">
         <ul class="nav navbar-nav">
             <li class="nav-item">
-                <b></b><a class="nav-link active" href="<?php echo $url_base ?>libs/login.php" aria-current="page">Inicio<span class="visually-hidden">(current)</span></a>
+              <a class="nav-link active" href="<?php echo $url_base ?>libs/login.php" aria-current="page">Inicio<span class="visually-hidden">(current)</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo $url_base?>secciones/stock">Stock</a>
