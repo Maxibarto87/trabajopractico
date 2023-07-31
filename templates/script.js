f8aa6e47401c483c646ab6da571cf18736f81f00
Maxibarto87 <barto_02_07_87@outlook.com>
2023-07-31 16:29:28 -0300

    actualiza el javascript

--- templates/script.js
+++ templates/script.js
@@ -3,6 +3,10 @@ let barraNavegacion = document.querySelectorAll('.nav-item');
 for (const unTitulo of barraNavegacion) {
 console.log(unTitulo);
 }
+
+let laAdministracion = document.querySelector('.administracion');
+console.log(laAdministracion);
+
 let elH1 = document.querySelectorAll('h1');
 for (const losH1 of elH1) {
     console.log(losH1);
@@ -10,4 +14,14 @@ for (const losH1 of elH1) {
 let  elInicio = document.querySelector('.palabra');
 console.log(elInicio);
 
+let  redesSociales = document.querySelectorAll('.links');
+for (const unRedes of redesSociales) {
+    console.log(unRedes);
+}
+
+let elFooter = document.querySelector('.footer');
+console.log(elFooter);
+
+let elParrafo = document.querySelector('.parrafo');
+console.log(elParrafo);
 
